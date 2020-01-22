ee0e15381bf2cb785537da1fb4865573243d586e
Daniel Baquero <835@holbertonschool.com>
2020-01-22 18:29:37 +0000

    [Feature] Had to add another decorator with the default values

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Displays Greeting HBNB! """
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Displays HBNB with route /hbnb """
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    """ Diplays C and the text entered as the route """
+    return ('C %s' % text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': "is cool"})
+@app.route('/python/<text>', strict_slashes=False)
+def hello_python(text):
+    """ Displays python and the text entered as the route or 'is cool' """
+    return ('Python %s' % text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
