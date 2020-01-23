dfb203a8686afc0c46270d596db07d3a6755de5d
Daniel Baquero <835@holbertonschool.com>
2020-01-23 00:56:46 +0000

    [Fixed] Fixing in process

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,19 +9,17 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_session(exception):
+def teardown_session(exception):
     """ Removes the current SQLAlchemy Session """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def display_html():
-    """ Displays html page within the body tag """
-    states = storage.all('State')
-    dic_obj = {item.id: item.name for item in states.values()}
-    return (render_template('7-states_list.html',
-                            Table="States",
-                            items=dic_obj)
+    """ Function called with /states_list route """
+    states = storage.all(State)
+    dic_obj = {value.id: value.name for value in states.values()}
+    return render_template('7-states_list.html', items=dic_obj)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
